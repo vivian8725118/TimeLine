696b18f1daccfb06fc9e9517d6f33d66f3b31cf1
Cyb3rKo <niko@cyb3rko.de>
2020-09-01 16:53:30 +0200

    updated test class

--- app/src/androidTest/java/com/vivian/timeline/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/vivian/timeline/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
 package com.vivian.timeline;
 
 import android.content.Context;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,12 +15,12 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
         assertEquals("com.vivian.timeline", appContext.getPackageName());
     }
